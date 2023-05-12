ca2bab4cefb205ad37e6ceb62469631ba5926f80
alecwu44743 <alec.wu44743@gmail.com>
2023-05-12 22:28:49 +0800

    Feat. Index page update

---
+++ booking/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from datetime import datetime, timedelta
+from .models import *
+from django.contrib import messages
+
+def index(request):
+    return render(request, "index.html",{})
